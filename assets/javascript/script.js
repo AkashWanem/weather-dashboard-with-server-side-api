27d855d7d98f2fddec7a34a7058f3fd85af03cb0
Akash Limbu <akashwanem@hotmail.com>
2022-06-01 14:36:31 +0100

    added function that fetches all the current data

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -1,10 +1,29 @@
-const APIKey = "383591663bd24388736a2cac0e243d50";
+const APIKey = "bd981e7e6881f3961e89d309552d15b6";
 const currentDate = moment().format("(L)");
 const currentCity = "";
 const previousCity = "";
 
 // get current weather data
-function currentWeather (city){
-    let queryURL = `https://api.openweathermap.org/data/2.5/weather?lat={lat}&lon={lon}&appid={API key}`;
-    
-}
+function currentWeather(city) {
+    let queryURL = `https://api.openweathermap.org/data/3.0/onecall?lat={lat}&lon={lon}&exclude={part}&appid={APIkey}`;
+    fetch(queryURL)
+    .then((response) => {
+        return response.json();
+    })
+    .then((response) => {
+        $('#city').innerHTML = response.data.name + " " + currentDate;
+        let weatherIcon = response.data.weather[0].icon;
+        $('#weather-icon').setAttribute("src","https://openweathermap.org/img/wn/" + weatherIcon + "@2x.png");
+        let currentTemp = data.main.temp;
+        let celsiusValue = currentTemp - 273.15;
+        $('#temp').innerHTML = "Temperature:" + " " + Math.floor(celsiusValue) + "°C";
+        $('#wind').innerHTML = "Wind Speed: " + response.data.wind.speed + " MPH";
+        $('#humid').innerHTML = "Humidity: " + response.data.main.humidity + "%";
+    })
+};
+
+// event listener
+$('#search-btn').on("click", (event) => {
+    event.preventDefault();
+    currentWeather(city);
+});
