58b0cfc6f8abd6d7453b13ac5a1cb938a34a0ea4
Akash Limbu <akashwanem@hotmail.com>
2022-06-01 13:17:15 +0100

    imported moment to get current date and time

---
+++ assets/javascript/script.js
@@ -0,0 +1,10 @@
+const APIKey = "383591663bd24388736a2cac0e243d50";
+const currentDate = moment().format("(L)");
+const currentCity = "";
+const previousCity = "";
+
+// get current weather data
+function currentWeather (city){
+    let queryURL = `https://api.openweathermap.org/data/2.5/weather?lat={lat}&lon={lon}&appid={API key}`;
+    
+}
